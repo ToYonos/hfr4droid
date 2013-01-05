9a5e11a6037686c13b3110ddaa37b673973a37ee
philippe.serre <philippe.serre@ec45ad31-c668-5095-c9a5-688cd4ab0ba3>
2013-01-05 00:04:44 +0000

    Réorga version donate

--- hfr4droidDonate/src/info/toyonos/hfr4droid/donate/activity/SplashActivity.java
+++ hfr4droidDonate/src/info/toyonos/hfr4droid/donate/activity/SplashActivity.java
@@ -1,10 +1,10 @@
 package info.toyonos.hfr4droid.donate.activity;
 
+import info.toyonos.hfr4droid.common.core.bean.Theme;
 import info.toyonos.hfr4droid.donate.R;
-import info.toyonos.hfr4droid.lib.core.bean.Theme;
 import android.widget.TextView;
 
-public class SplashActivity extends info.toyonos.hfr4droid.lib.activity.SplashActivity
+public class SplashActivity extends info.toyonos.hfr4droid.common.activity.SplashActivity
 {
 	@Override
 	protected void applyTheme(Theme theme)
