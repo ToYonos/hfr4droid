307a5415257f61c7081ab9bb6c087f72020611dd
philippe.serre <philippe.serre@ec45ad31-c668-5095-c9a5-688cd4ab0ba3>
2013-07-19 12:29:13 +0000

    tag 0.8.0

---
+++ 0.8.0/src/info/toyonos/hfr4droid/common/activity/NewPostGenericActivity.java
@@ -0,0 +1,171 @@
+package info.toyonos.hfr4droid.common.activity;
+
+import info.toyonos.hfr4droid.common.R;
+import info.toyonos.hfr4droid.common.core.bean.Theme;
+import info.toyonos.hfr4droid.common.util.helper.NewPostUIHelper;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnDismissListener;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+/**
+ * <p>Activity abstraite permettant d'ajouter un topic ou un post</p>
+ * 
+ * @author ToYonos
+ *
+ */
+public abstract class NewPostGenericActivity extends HFR4droidActivity
+{	
+	/**
+	 * La fenêtre de dialog pour choisir les smilies
+	 */
+	private Dialog smiliesDialog; 
+	
+	/**
+	 * le helper pour gérer le comportement de l'interface
+	 */
+	protected NewPostUIHelper uiHelper;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		uiHelper = getNewPostUIHelper();
+	}
+	
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		if (!isLoggedIn()) showLoginDialog(true);	
+	}
+	
+	@Override
+	protected void onPause()
+	{
+		super.onPause();
+		if (smiliesDialog != null) smiliesDialog.dismiss();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) 
+	{
+		if (keyCode == KeyEvent.KEYCODE_BACK)
+		{
+			ViewGroup layout = (ViewGroup) findViewById(R.id.PostContainer);
+			if (layout.findViewById(R.id.SmileySearch).getVisibility() == View.VISIBLE)
+			{
+				uiHelper.hideWikiSmiliesSearch(layout);
+				return true;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	protected void goBack()
+	{
+		finish();
+	}
+	
+	@Override
+	protected void onLogout()
+	{
+		loadCats(false);
+	}
+	
+	@Override
+	protected void applyTheme(Theme theme)
+	{
+		LinearLayout root = (LinearLayout) findViewById(R.id.NewPostGenericRoot);
+		root.setBackgroundColor(theme.getListBackgroundColor());
+		
+		uiHelper.applyTheme(theme, (ViewGroup) findViewById(R.id.PostContainer).getParent());
+		
+		if (smiliesDialog != null) applyThemeForSmilies(theme);
+	}
+	
+	private void applyThemeForSmilies(Theme theme)
+	{
+		LinearLayout smilies = (LinearLayout) smiliesDialog.findViewById(R.id.SmiliesContainer);
+		smilies.setBackgroundColor(theme.getListBackgroundColor());
+	}
+	
+	protected abstract void setOkButtonClickListener(Button okButton);
+	
+	private NewPostUIHelper getNewPostUIHelper()
+	{
+		return new NewPostUIHelper()
+		{
+			@Override
+			protected void showWikiSmiliesResults(ViewGroup layout)
+			{
+				smiliesDialog.show();
+			}
+			
+			@Override
+			public void hideWikiSmiliesResults(ViewGroup layout)
+			{
+				smiliesDialog.dismiss();
+				destroyWikiSmiliesResults(layout);
+			}
+
+			@Override
+			protected void setOkButtonClickListener(Button okButton)
+			{
+				NewPostGenericActivity.this.setOkButtonClickListener(okButton);
+			}
+			
+			@Override
+			protected void onRehostOk(String url)
+			{
+				insertBBCode((EditText) findViewById(R.id.InputPostContent), url, "");
+			}
+
+			
+			@Override
+			public ViewGroup getSmiliesLayout()
+			{
+				if (smiliesDialog == null)
+				{
+					smiliesDialog = new Dialog(NewPostGenericActivity.this);
+					smiliesDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+					LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+					final ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.smilies, null);
+					smiliesDialog.setContentView(layout);
+					applyThemeForSmilies(currentTheme);
+					
+					smiliesDialog.setOnDismissListener(new OnDismissListener()
+					{
+						public void onDismiss(DialogInterface dialog)
+						{
+							destroyWikiSmiliesResults(layout);
+						}
+					});
+					
+					return layout;
+				}
+				else
+				{
+					return (ViewGroup) smiliesDialog.findViewById(R.id.SmiliesContainer);
+				}
+			}
+		};
+	}
+}
---
+++ 0.8.0/src/info/toyonos/hfr4droid/common/activity/PostsSearchActivity.java
@@ -0,0 +1,272 @@
+package info.toyonos.hfr4droid.common.activity;
+
+import info.toyonos.hfr4droid.common.R;
+import info.toyonos.hfr4droid.common.core.bean.Post;
+import info.toyonos.hfr4droid.common.core.bean.PostFromSearch;
+import info.toyonos.hfr4droid.common.core.data.DataRetrieverException;
+import info.toyonos.hfr4droid.common.core.data.HFRUrlParser;
+import info.toyonos.hfr4droid.common.core.data.MDUrlParser;
+import info.toyonos.hfr4droid.common.core.message.HFRMessageSender.ResponseCode;
+import info.toyonos.hfr4droid.common.util.helper.NewPostUIHelper;
+import info.toyonos.hfr4droid.common.util.listener.OnScreenChangeListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SlidingDrawer;
+import android.widget.TextView;
+
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.SubMenu;
+import com.naholyr.android.ui.HFR4droidQuickActionWindow;
+import com.naholyr.android.ui.QuickActionWindow;
+import com.naholyr.android.ui.QuickActionWindow.Item;
+
+/**
+ * <p>Activity permettant la recherche de posts intra topic</p>
+ * 
+ * @author ToYonos
+ *
+ */
+public class PostsSearchActivity extends PostsActivity
+{
+	private List<Post> fromPosts;
+	private String pseudo;
+	private String word;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void onCreateInit(Bundle bundle)
+	{
+		// Listener pour le changement de view dans le composant DragableSpace
+		space.setOnScreenChangeListener(new OnScreenChangeListener()
+		{
+			public void onScreenChange(int oldIndex, int newIndex){}
+			
+			public void onFailRearward()
+			{
+				if (currentPageNumber != 1) loadPreviousPage();
+			}
+			
+			public void onFailForward()
+			{
+				loadNextPage();
+			}
+		});
+		
+		fromPosts = new ArrayList<Post>();
+		Post fromPost = bundle != null ? (Post) bundle.getSerializable("fromPost") : new Post(0);
+		currentPageNumber = 1;
+		addFromPost(fromPost);
+		
+		pseudo = bundle != null ? bundle.getString("pseudo") : null;
+		word = bundle != null ? bundle.getString("word") : null;
+		
+		if (bundle != null && bundle.getSerializable("posts") != null)
+		{
+			List<Post> posts = setDatasource((List<Post>) bundle.getSerializable("posts"));
+			if (posts != null && posts.size() > 0)
+			{
+				topic = posts.get(0).getTopic();
+				Post lastPost = posts.get(posts.size() - 1);
+				addFromPost(lastPost);
+				displayPosts(posts);
+			}
+		}
+		
+		EditText pseudoET = (EditText) findViewById(R.id.SearchPostsPseudo);
+		pseudoET.setText(pseudo);
+		EditText wordET = (EditText) findViewById(R.id.SearchPostsWord);
+		wordET.setText(word);
+	}
+	
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		LinearLayout searchPanel = (LinearLayout) findViewById(R.id.SearchPostsPanel);
+		searchPanel.setVisibility(View.VISIBLE);
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) 
+	{
+		if (keyCode == KeyEvent.KEYCODE_SEARCH)
+		{
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu)
+	{
+		super.onPrepareOptionsMenu(menu);
+
+		SubMenu subMenuNav = menu.findItem(R.id.MenuNav).getSubMenu();
+
+		MenuItem menuNavFP = subMenuNav.findItem(R.id.MenuNavFirstPage);
+		menuNavFP.setVisible(currentPageNumber != 1);
+		menuNavFP.setEnabled(currentPageNumber != 1);
+
+		MenuItem menuNavPP = subMenuNav.findItem(R.id.MenuNavPreviousPage);
+		menuNavPP.setVisible(currentPageNumber != 1);
+		menuNavPP.setEnabled(currentPageNumber != 1);
+		
+		MenuItem menuNavUP = subMenuNav.findItem(R.id.MenuNavUserPage);
+		menuNavUP.setVisible(false);
+		menuNavUP.setEnabled(false);
+
+		MenuItem menuNavNP = subMenuNav.findItem(R.id.MenuNavNextPage);
+		menuNavNP.setVisible(true);
+		menuNavNP.setEnabled(true);
+
+		MenuItem menuNavLP = subMenuNav.findItem(R.id.MenuNavLastPage);
+		menuNavLP.setVisible(false);
+		menuNavLP.setEnabled(false);
+
+		MenuItem refresh = menu.findItem(R.id.MenuRefresh);
+		refresh.setVisible(false);
+		refresh.setEnabled(false);
+
+		return true;
+	}
+
+	@Override
+	protected void setTitle()
+	{
+		final TextView topicTitle = (TextView) findViewById(R.id.TopicTitle);
+		topicTitle.setTextSize(getTextSize(15));
+		topicTitle.setText(topic.toString());
+		topicTitle.setSelected(true);
+		final TextView topicPageNumber = (TextView) findViewById(R.id.TopicPageNumber);
+		topicPageNumber.setTextSize(getTextSize(15));
+		topicPageNumber.setText("P." + currentPageNumber);
+	}
+
+	@Override
+	protected void loadFirstPage()
+	{
+		searchPosts(topic, pseudo, word, fromPosts.get(0));
+	}
+
+	@Override
+	protected void loadPreviousPage()
+	{
+		searchPosts(topic, pseudo, word, fromPosts.get(currentPageNumber - 2));
+	}
+
+	@Override
+	protected void loadNextPage()
+	{
+		searchPosts(topic, pseudo, word, fromPosts.get(currentPageNumber));
+	}
+
+	@Override
+	protected void reloadPage() {}
+
+	@Override
+	protected void goBack()
+	{
+		finish();
+	}
+
+	@Override
+	protected void onPostingOk(ResponseCode code, long postId)
+	{
+		switch (code)
+		{	
+			case POST_EDIT_OK: // Edit ok
+				jumpToPost(postId);
+				break;
+
+			case POST_ADD_OK: // New post ok
+				topic.setLastReadPost(NewPostUIHelper.BOTTOM_PAGE_ID);
+				loadPosts(topic, topic.getNbPages(), false);
+				break;
+		}
+	}
+
+	protected void addQuickActionWindowItems(HFR4droidQuickActionWindow window, final long currentPostId, boolean isMine)
+	{
+		QuickActionWindow.Item goToOriginalPost = new QuickActionWindow.Item(PostsSearchActivity.this, "", R.drawable.ic_menu_jump_to, new QuickActionWindow.Item.Callback()
+		{	
+			public void onClick(QuickActionWindow window, Item item, View anchor)
+			{
+				keepNavigationHistory = true;
+				jumpToPost(currentPostId);
+			}
+		});					
+		window.addItem(goToOriginalPost);
+
+		super.addQuickActionWindowItems(window, currentPostId, isMine);
+	}
+	
+	protected Post getCurrentFromPost()
+	{
+		return fromPosts.get(currentPageNumber - 1);
+	}
+	
+	private void jumpToPost(long postId)
+	{
+		try
+		{
+			PostFromSearch p = (PostFromSearch) getPostById(postId);
+			MDUrlParser urlParser = new HFRUrlParser(getDataRetriever());
+			if (urlParser.parseUrl(p.getCallbackUrl()))
+			{
+				topic.setLastReadPost(postId);
+				loadPosts(topic, urlParser.getPage(), false);
+			}
+		}
+		catch (DataRetrieverException e)
+		{
+			error(getString(R.string.error_dispatching_url), e, true, false);
+		}
+	}
+	
+	protected void addFromPost(Post p)
+	{
+		fromPosts.add(p);
+	}
+
+	protected Post getLastFromPost()
+	{
+		int size = fromPosts.size();
+		return size > 0 ? fromPosts.get(size - 1) : null;
+	}
+	
+	protected void setPageNumberFromPost(Post p)
+	{
+		int index = fromPosts.indexOf(p);
+		currentPageNumber = index != -1 ? index + 1 : 1;
+	}
+
+	@Override
+	protected void attachEvents()
+	{
+		super.attachEvents();
+		
+		final TextView topicTitle = (TextView) findViewById(R.id.TopicTitle);
+		topicTitle.setOnLongClickListener(null);
+	}
+	
+	@Override
+	public void preloadPosts(boolean verify)
+	{
+		// Rien à faire ici
+	}
+}
