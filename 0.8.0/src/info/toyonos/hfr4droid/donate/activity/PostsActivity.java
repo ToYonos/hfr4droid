2182042621ac84304f9f4d2ac4ef1d257b9f8243
philippe.serre <philippe.serre@ec45ad31-c668-5095-c9a5-688cd4ab0ba3>
2013-07-19 12:30:27 +0000

    tag 0.8.0

---
+++ 0.8.0/src/info/toyonos/hfr4droid/donate/activity/PostsActivity.java
@@ -0,0 +1,24 @@
+package info.toyonos.hfr4droid.donate.activity;
+
+import android.widget.EditText;
+import android.widget.Toast;
+import info.toyonos.hfr4droid.donate.R;
+import info.toyonos.hfr4droid.common.core.message.HFRMessageSender.ResponseCode;
+
+public class PostsActivity extends info.toyonos.hfr4droid.common.activity.PostsActivity
+{
+	protected void onPostingOk(ResponseCode code, long postId)
+	{
+		EditText postContent = (EditText) postDialog.findViewById(R.id.InputPostContent);
+		String content = postContent.getText().toString();
+
+		switch (code)
+		{	
+			case POST_ADD_OK: // Easter Egg :)
+				if (content.toLowerCase().contains(getString(R.string.god))) Toast.makeText(this, R.string.thankyou_respect, Toast.LENGTH_LONG).show();
+
+			default:
+				super.onPostingOk(code, postId);
+		}
+	}
+}
